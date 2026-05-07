46df91aca039cfb9170038099d3aaf853d9ecfc5
semoljeh <aromtemplate@gmail.com>
2026-05-07 10:44:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,8 @@
-const CACHE_NAME = 'darussalam-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // Service worker harus memiliki event fetch agar dianggap PWA valid
+  event.respondWith(fetch(event.request));
 });
