4021d5abc0a3fc8f141a3d5951594c45968bf037
semoljeh <aromtemplate@gmail.com>
2026-05-09 00:54:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,27 @@
-const CACHE_NAME = 'semoljeh-v2-offline';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
+const CACHE_NAME = 'madasa-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  './LPMQ IsepMisbah.ttf', // Font lokal Anda
   'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@700&display=swap',
-  'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400&display=swap',
-  'https://kemenag.go.id/assets/fonts/lpmq.ttf'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
   );
 });
