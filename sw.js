b7bc57bb8520e1a932a6cd2ebefd2b9cf4d040a3
semoljeh <aromtemplate@gmail.com>
2026-05-09 00:56:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
 const CACHE_NAME = 'madasa-v1';
 const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  './LPMQ IsepMisbah.ttf', // Font lokal Anda
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './LPMQ IsepMisbah.ttf', 
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
+// Tahap Install: Download semua file utama
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      console.log('Mendownload data aplikasi ke cache...');
+      return cache.addAll(assets);
     })
   );
 });
 
+// Tahap Fetch: Ambil dari cache jika offline
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
