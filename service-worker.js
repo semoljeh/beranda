d291f75073369b8e4bf3bba2aeb9caadb0ebe2cc
semoljeh <aromtemplate@gmail.com>
2026-05-13 22:00:39 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,51 +2,73 @@ const CACHE_NAME = "semoljeh-v2.0.1";
 
 const urlsToCache = [
   "./",
-  "./index.html",
   "./manifest.json",
-  "./notif.mp3"
+  "./notif.mp3",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 // INSTALL
 self.addEventListener("install", event => {
+
   self.skipWaiting();
 
   event.waitUntil(
+
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+
   );
+
 });
 
 // ACTIVATE
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(keys => {
+
       return Promise.all(
+
         keys.map(key => {
+
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
+
         })
+
       );
+
     })
+
   );
 
   self.clients.claim();
+
 });
 
 // FETCH
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+
+    fetch(event.request)
+
+      .then(response => response)
+
+      .catch(() => caches.match(event.request))
+
   );
+
 });
 
 // FORCE UPDATE
-self.addEventListener('message', event => {
-  if (event.data.action === 'skipWaiting') {
+self.addEventListener("message", event => {
+
+  if (event.data.action === "skipWaiting") {
     self.skipWaiting();
   }
+
 });
