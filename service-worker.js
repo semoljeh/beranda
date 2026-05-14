e9dcae56f2eb4c93691d44be24953eddfda0a781
semoljeh <aromtemplate@gmail.com>
2026-05-14 13:01:56 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,86 @@
-const CACHE_NAME = "semoljeh-v2.0.15";
+const CACHE_NAME = 'semoljeh-cache-v2';
 
 const urlsToCache = [
-  "./",
-  "./manifest.json",
-  "./notif.mp3",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://raw.githubusercontent.com/semoljeh/beranda/main/icon-192.png'
 ];
 
 // INSTALL
-self.addEventListener("install", event => {
-
-
-
+self.addEventListener('install', event => {
   event.waitUntil(
-
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-
   );
 
+  self.skipWaiting();
 });
 
 // ACTIVATE
-self.addEventListener("activate", event => {
-
+self.addEventListener('activate', event => {
   event.waitUntil(
-
     caches.keys().then(keys => {
-
       return Promise.all(
-
         keys.map(key => {
-
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
-
         })
-
       );
-
     })
-
   );
 
   self.clients.claim();
-
 });
 
 // FETCH
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
+
+  if (event.request.method !== 'GET') return;
 
   event.respondWith(
 
-    fetch(event.request)
+    caches.match(event.request)
+      .then(response => {
 
-      .then(response => response)
+        if (response) {
+          return response;
+        }
 
-      .catch(() => caches.match(event.request))
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            if (
+              !networkResponse ||
+              networkResponse.status !== 200 ||
+              networkResponse.type !== 'basic'
+            ) {
+              return networkResponse;
+            }
+
+            const responseClone = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseClone);
+              });
+
+            return networkResponse;
+          })
+          .catch(() => {
+
+            // fallback offline
+            if (event.request.destination === 'document') {
+              return caches.match('./index.html');
+            }
+
+          });
+
+      })
 
   );
-
-});
-
-// FORCE UPDATE
-self.addEventListener("message", event => {
-
-  if (event.data.action === "skipWaiting") {
-    self.skipWaiting();
-  }
 
 });
